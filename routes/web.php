fc33d24fb1b70356dccc8f63b85e0c0d4e00a7c2
VladimirKozachenko <vladimir.kozachenko@catenamedia.com>
2019-10-16 16:13:40 +0300

    install laravel and add form

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+
+Route::group(['namespace' => 'Client'], function (){
+    Route::get('/client', 'ClientController@index')->name('client');
+    Route::post('/check', 'ClientController@checkClient')->name('check');
+});
